dde37beb682eb406ca81d7e15a035a36b69d1911
Vanshikagupta653 <vanshikaseema653@gmail.com>
2025-08-17 22:12:40 +0530

    Create userinputExample.java

---
+++ userinputExample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class UserInputExample {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
