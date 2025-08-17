bcdae020b7e821b1f39ad3a976b00947d015da8d
Vanshikagupta653 <vanshikaseema653@gmail.com>
2025-08-17 22:13:30 +0530

    Create AddTwoNumber.java

---
+++ AddTwoNumber.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5; // First number
+        int num2 = 7; // Second number
+
+        int sum = num1 + num2; // Adding the two numbers
+
+        System.out.println("The sum is: " + sum);
+    }
+}
