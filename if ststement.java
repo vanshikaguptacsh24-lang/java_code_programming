5ebb2d3e5b540f0e8273313f60614c6188e2fc5e
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-20 16:59:21 +0530

    Create if ststement.java

---
+++ if ststement.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // Input number
+
+        if (n > 100) {
+            System.out.println("Big");   // Print Big if condition is true
+        }
+
+        System.out.println("Number");    // Always print Number
+    }
+}
