ebee785f90052b00c94e70f17e64b8306a5b34bd
Vanshikagupta653 <vanshikaseema653@gmail.com>
2025-08-23 13:11:35 +0530

    Create hello .java

---
+++ hello .java
@@ -0,0 +1,10 @@
+class Test {
+    static void hello() {
+        System.out.println("Hello");
+    }
+}
+public class Geeks {
+    public static void main(String[] args) {
+        Test.hello(); // Call static method directly
+    }
+}
