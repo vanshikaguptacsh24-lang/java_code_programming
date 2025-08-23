e30df586b3f80f11cdbe97048dd08256f67d2a92
Vanshikagupta653 <vanshikaseema653@gmail.com>
2025-08-23 13:16:36 +0530

    Create creat_object_call_method.java

---
+++ creat_object_call_method.java
@@ -0,0 +1,10 @@
+class Geeks {
+    void hello() {
+        System.out.println("This is a user-defined method.");
+    }
+
+    public static void main(String[] args) {
+        Geeks obj = new Geeks(); // Create object
+        obj.hello();             // Call method
+    }
+}
