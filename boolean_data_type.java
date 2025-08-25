9210ecff92da64cac86f5990f14edcc623ef3d0a
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-25 22:08:55 +0530

    Create boolean_data_type.java

---
+++ boolean_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating boolean data type
+public class Geeks {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println("Is Java fun? " + b1);  
+        System.out.println("Is fish tasty? " + b2); 
+    }
+}
