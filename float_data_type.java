8cf93d1f52ade66b46565d8c85fac9ae431b0817
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:26:19 +0530

    Create float_data_type.java

---
+++ float_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating float data type
+public class Geeks {
+    public static void main(String[] args) {
+        float pi = 3.14f;
+        float gravity = 9.81f;
+
+        System.out.println("Value of Pi: " + pi);  
+        System.out.println("Gravity: " + gravity); 
+    }
+}
