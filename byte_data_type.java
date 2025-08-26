ab0eabd4b717075ddd4dedb4da7fd5f443a8a227
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:23:40 +0530

    Create byte_data_type.java

---
+++ byte_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating byte data type
+public class Geeks {
+    public static void main(String[] args) {
+        byte a = 25;
+        byte t = -10;
+
+        System.out.println("Age: " + a);  
+        System.out.println("Temperature: " + t); 
+    }
+}
