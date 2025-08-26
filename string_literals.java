65f2e214e91af9bc1a46c273699544f4d16b745f
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 22:05:36 +0530

    Create string_literals.java

---
+++ string_literals.java
@@ -0,0 +1,15 @@
+// Java program to illustrate the 
+// application of String literals
+public class Geeks {
+    public static void main(String[] args) {
+        String s = "Hello";
+
+        // Without double quotes, it is treated as a variable and causes a compiler error
+        // String s1 = Hello; 
+
+        System.out.println(s);
+        
+        // commented out due to error
+        // System.out.println(s1);
+    }
+}
