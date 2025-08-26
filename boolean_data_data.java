c10e9df9b3cd7d66abb359a780b2fe3470a61540
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 22:09:57 +0530

    Create boolean_data_data.java

---
+++ boolean_data_data.java
@@ -0,0 +1,20 @@
+// Java program to illustrate the 
+// application of boolean literals
+public class Geeks{
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean c = false;
+
+        // The following lines cause compilation 
+        // errors and are commented out
+        
+        // boolean d = 0; 
+        // boolean e = 1;
+
+        System.out.println(b);
+        System.out.println(c);
+        
+        // System.out.println(d);
+        // System.out.println(e);
+    }
+}
