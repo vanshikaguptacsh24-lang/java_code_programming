e8afea78ec5f7924ab7980681acc4db4105707ab
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:21:34 +0530

    Create illustrate.java

---
+++ illustrate.java
@@ -0,0 +1,12 @@
+// Java Program to illustrate what if
+// we use the keywords as the variable name
+class Geeks 
+{
+    public static void main(String[] args)
+    {
+        // Note "this" is a reserved
+        // word in java
+        String this = "Hello World!";
+        System.out.println(this);
+    }
+}
