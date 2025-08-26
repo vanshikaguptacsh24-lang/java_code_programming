4665c548e14aaad91d21b98360f4d72e1f3ec278
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 22:59:38 +0530

    Create Widening TypeCasting.java

---
+++ Widening TypeCasting.java
@@ -0,0 +1,21 @@
+// Java program to demonstrate Widening TypeCasting
+import java.io.*;
+
+class Geeks {
+    public static void main(String[] args)
+    {
+        int i = 10;
+
+        // Wideing TypeCasting (Automatic Casting)
+        // from int to long
+        long l = i;
+
+        // Wideing TypeCasting (Automatic Casting)
+        // from int to double
+        double d = i;
+
+        System.out.println("Integer: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Double: " + d);
+    }
+}
