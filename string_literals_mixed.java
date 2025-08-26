a436ce2af34ee114e75d27d3153444f4c6bf1bf7
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 22:21:25 +0530

    Create string_literals_mixed.java

---
+++ string_literals_mixed.java
@@ -0,0 +1,16 @@
+// Java program to illustrate the behaviour of
+// char literals and integer literals when
+// we are performing addition
+
+public class Geeks {
+    public static void main(String[] args)
+    {
+        // ASCII value of 0 is 48
+        int first = '0';
+
+        // ASCII value of 7 is 55
+        int second = '7';
+        System.out.println("Geeks!" + first +
+                                '2' + second);
+    }
+}
