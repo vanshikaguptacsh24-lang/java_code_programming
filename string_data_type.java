c148fb5d4d2dd040f0a41187d83345e44f94898a
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:29:49 +0530

    Create string_data_type.java

---
+++ string_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating String data type
+public class Geeks {
+    public static void main(String[] args) {
+        String n = "Geek1";
+        String m = "Hello, World!";
+
+        System.out.println("Name: " + n); 
+        System.out.println("Message: " + m); 
+    }
+}
