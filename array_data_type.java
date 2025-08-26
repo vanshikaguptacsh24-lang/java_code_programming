baaface715450842c8c00ce35d3444c9fbdc043a
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:32:51 +0530

    Create array_data_type.java

---
+++ array_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating how to create an array
+public class Geeks {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5};
+        String[] arr = {"Geek1", "Geek2", "Geek3"};
+
+        System.out.println("First Number: " + num[0]);  
+        System.out.println("Second Fruit: " + arr[1]);
+    }
+}
