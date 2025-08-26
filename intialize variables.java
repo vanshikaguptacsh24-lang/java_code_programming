11b3d89f67b3955609ab0d467457dd0a9081c9d6
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 22:39:45 +0530

    Create intialize variables.java

---
+++ intialize variables.java
@@ -0,0 +1,24 @@
+// Demonstrating how to intialize variables
+// of different types in Java
+
+class Geeks{
+    public static void main(String[] args) {
+        // Declaring and initializing variables
+        
+        // Initializing float variable
+        float si = 5.5f; 
+        
+        // Initializing integer variables
+        int t = 10; 
+        int s = 20; 
+        
+        // Initializing character variable
+        char var = 'h';               
+
+        // Displaying the values of the variables
+        System.out.println("Simple Interest: " + si);  
+        System.out.println("Speed: " + s);                      
+        System.out.println("Time: " + t);                        
+        System.out.println("Character: " + var);                
+    }
+}
