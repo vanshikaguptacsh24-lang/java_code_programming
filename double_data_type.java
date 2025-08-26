4e96706978fe88684ed9f26a509946bc4a2e3ff4
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:27:25 +0530

    Create double_data_type.java

---
+++ double_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating double data type
+public class Geeks {
+    public static void main(String[] args) {
+        double pi = 3.141592653589793;
+        double an = 6.02214076e23;
+
+        System.out.println("Value of Pi: " + pi);  
+        System.out.println("Avogadro's Number: " + an); 
+    }
+}
