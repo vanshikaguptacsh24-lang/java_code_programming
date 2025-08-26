07495703279393860a477a25f6289a2702f0f66b
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:28:07 +0530

    Create char_data_type.java

---
+++ char_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating char data type
+public class Geeks{
+    public static void main(String[] args) {
+        char g = 'A';
+        char s = '$';
+
+        System.out.println("Grade: " + g); 
+        System.out.println("Symbol: " + s); 
+    }
+}
