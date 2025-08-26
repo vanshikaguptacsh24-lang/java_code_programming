44a1ee37775e7224cb45e368f2093e0d9d93dc7b
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:25:45 +0530

    Create long_data_type.java

---
+++ long_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating long data type
+public class Geeks {
+    public static void main(String[] args) {
+        long w = 7800000000L;
+        long l = 9460730472580800L;
+
+        System.out.println("World Population: " + w);  
+        System.out.println("Light Year Distance: " + l);
+    }
+}
