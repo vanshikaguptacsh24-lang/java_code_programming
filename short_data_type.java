13bd10acf987d01286618f0b17bf042c24c9470d
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:24:17 +0530

    Create short_data_type.java

---
+++ short_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating short data types
+public class Geeks {
+    public static void main(String[] args) {
+        short num = 1000;
+        short t = -200;
+
+        System.out.println("Number of Students: " + num);  
+        System.out.println("Temperature: " + t); 
+    }
+}
