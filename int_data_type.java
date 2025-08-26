1d28f77f8411f6dad3453b40958e3fd3a4264e75
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:24:57 +0530

    Create int_data_type.java

---
+++ int_data_type.java
@@ -0,0 +1,10 @@
+// Demonstrating int data types
+public class Geeks {
+    public static void main(String[] args) {
+        int p = 2000000;
+        int d = 150000000;
+
+        System.out.println("Population: " + p); 
+        System.out.println("Distance: " + d);
+    }
+}
