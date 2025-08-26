a07762ea18e04284f8ec2f6cecfb9898b7126793
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 22:28:34 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,21 @@
+// Demonstarting how to declare and use a variable in Java
+
+class Geeks {
+    public static void main(String[] args) {
+        // Declaring and initializing variables
+        
+        // Integer variable  
+        int age = 25;       
+        
+        // String variable  
+        String name = "GeeksforGeeks"; 
+        
+        // Double variable 
+        double salary = 50000.50;     
+
+        // Displaying the values of variables
+        System.out.println("Age: " + age);          
+        System.out.println("Name: " + name);        
+        System.out.println("Salary: " + salary);
+    }
+}
