f078a1d734db85e80c3c553f2c6c18d07841ff94
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 21:46:10 +0530

    Create literal.java

---
+++ literal.java
@@ -0,0 +1,18 @@
+public class Geeks {
+    public static void main(String[] args)
+    {
+          // decimal-form literal
+        int a = 101; 
+          // octal-form literal
+        int b = 0100; 
+        // Hexa-decimal form literal
+        int c = 0xFace; 
+          // Binary literal
+        int d = 0b1111; 
+      
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
