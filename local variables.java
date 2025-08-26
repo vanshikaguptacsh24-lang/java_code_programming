e3ec70fe2e22dcb6c6f67a5ebbf668db8e16a98f
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-26 22:42:17 +0530

    Create local variables.java

---
+++ local variables.java
@@ -0,0 +1,13 @@
+// Java Program to show the use of local variables
+import java.io.*;
+
+class Geeks {
+    public static void main(String[] args)
+    {
+        // Declared a Local Variable
+        int var = 10;
+
+        // This variable is local to this main method only
+        System.out.println("Local Variable: " + var);
+    }
+}
