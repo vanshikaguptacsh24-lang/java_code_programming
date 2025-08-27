ebd123d1053ef60bad463cbcfbc38d0d77f9a686
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-27 22:43:21 +0530

    Create +_and_- operator.java

---
+++ operaters/+_and_- operator.java
@@ -0,0 +1,19 @@
+// Java program to show the use of + and - operators
+public class Geeks 
+{  
+    public static void main(String[] args) 
+    {
+      // Declare and initialize variables
+        int num1 = 500;
+        int num2 = 100;
+        
+        // Using the + (addition) operator
+        int sum = num1 + num2;
+        System.out.println("The Sum is: "+sum);
+        
+        // Using the - (subtraction) operator
+        int diff = num1 - num2;
+        System.out.println("The Difference is: "+diff);
+        
+    }
+}
