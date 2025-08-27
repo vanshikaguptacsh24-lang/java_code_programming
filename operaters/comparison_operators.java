4d9cb0d32a6109266cbcda262b42bf96803d36e1
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-27 23:00:45 +0530

    Create comparison_operators.java

---
+++ operaters/comparison_operators.java
@@ -0,0 +1,23 @@
+// Java Program to show the use of
+// Relational Operators
+import java.io.*;
+
+// Driver Class
+class Geeks {
+    
+    // main function
+    public static void main(String[] args)
+    {
+        // Comparison operators
+        int a = 10;
+        int b = 3;
+        int c = 5;
+
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a == c: " + (a == c));
+        System.out.println("a != c: " + (a != c));
+    }
+}
