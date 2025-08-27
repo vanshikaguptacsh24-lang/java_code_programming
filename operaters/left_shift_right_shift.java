eb44733be73d0d874b9b2ea68fca2f144dc6fe4d
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-27 23:14:13 +0530

    Create left_shift_right_shift.java

---
+++ operaters/left_shift_right_shift.java
@@ -0,0 +1,17 @@
+// Java Program to show the use of
+// shift operators
+import java.io.*;
+
+class Geeks 
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+    
+        // Using left shift
+        System.out.println("a<<1 : " + (a << 1));
+      
+        // Using right shift
+        System.out.println("a>>1 : " + (a >> 1));
+    }
+}
