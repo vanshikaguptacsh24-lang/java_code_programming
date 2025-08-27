a1957f8e8b153f2da59ce6ec0ddfc79097666c68
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-08-27 23:03:33 +0530

    Create logical_operators.java

---
+++ operaters/logical_operators.java
@@ -0,0 +1,18 @@
+// Java Program to show the use of
+// Logical operators
+import java.io.*;
+
+class Geeks {
+  
+      // Main Function
+    public static void main (String[] args) {
+      
+        // Logical operators
+        boolean x = true;
+        boolean y = false;
+      
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+    }
+}
