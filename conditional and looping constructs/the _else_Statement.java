e56f014e25a752f1d5e157ac8fdeddadcafc1b60
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-09-04 22:17:31 +0530

    Create the _else_Statement.java

---
+++ conditional and looping constructs/the _else_Statement.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Your Code Here
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        if (n>100){
+            System.out.println("Big");
+        } else {
+            System.out.println("Number");
+        }
+    }
+}
