c96fcb61caa7c61a3e9445d0ccd78d1d7e59bb5f
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-09-04 22:19:09 +0530

    Create Switch_statement.java

---
+++ conditional and looping constructs/Switch_statement.java
@@ -0,0 +1,40 @@
+// Back-end complete function Template for Java
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        int a = 5; // You can change this value to test other cases
+
+        switch (a) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+                System.out.println("Unknown");
+        }
+    }
+}
