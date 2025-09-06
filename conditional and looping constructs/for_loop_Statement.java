ebf2e405313a53b239afeb484393a8995ae51d01
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-09-06 11:16:01 +0530

    Create for_loop_Statement.java

---
+++ conditional and looping constructs/for_loop_Statement.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public static void solve(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1;i<=10;i++){
+            System.out.print((n*i)+" ");
+        }
+    }
+}
