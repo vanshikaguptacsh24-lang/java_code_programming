b0315f8323a58f1416748a0691c8805ddaeb18f3
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-09-06 17:35:34 +0530

    Create java_constructor.java

---
+++ oops_&_construct/java_constructor.java
@@ -0,0 +1,20 @@
+// Java Program to demonstrate
+// Constructor usage
+import java.io.*;
+
+// Driver Class
+class Geeks {
+  
+    // Constructor
+    Geeks()
+    {
+        super();
+        System.out.println("Constructor Called");
+    }
+
+    // main function
+    public static void main(String[] args)
+    {
+        Geeks geek = new Geeks();
+    }
+}
