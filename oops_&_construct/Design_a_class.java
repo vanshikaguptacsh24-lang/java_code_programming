463653f314e306420366415f70e08198ec0ef041
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-09-06 16:27:05 +0530

    Create Design_a_class.java

---
+++ oops_&_construct/Design_a_class.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+// Design a class as described in the question
+// Code here
+class MyClass {
+    public void display(){
+        System.out.println("Hello World");
+    }
+}
