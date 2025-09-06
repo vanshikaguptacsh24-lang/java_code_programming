6fc0bb259ad7368838a345deb50758c35acf5a19
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-09-06 13:10:21 +0530

    Create Length_of_string.java

---
+++ String/Length_of_string.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    // Complete the function
+    // str: input string
+    public static int lengthString(String str) {
+        // find the length of string
+        return str.length();
+    }
+}
