9f813b84a159e7dceab6f45d888785125eb7fee7
vanshikaguptacsh24-lang <vanshika.gupta_cs.h24@gla.ac.in>
2025-09-06 12:45:46 +0530

    Update and rename S.java to Decrement_Arrays_values.java.java

---
+++ Arrays/Decrement_Arrays_values.java.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static  int[] decrementArrayElements(int[] arr) {
+        // Code here
+        for(int i =0; i< arr.length; i++){
+            arr[i]=arr[i]-1;
+        }
+        return arr;
+        
+    }
+}
